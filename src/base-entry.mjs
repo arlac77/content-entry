68f7ee511b4da81300f52ba727e0426b5883a8ec
Markus Felten <Markus.Felten@gmx.de>
2018-12-13 11:27:32 +0100

    chore: initial checkin

---
+++ src/base-entry.mjs
@@ -0,0 +1,42 @@
+/**
+ * Representation of one file or directory entry
+ * All names are asolute (no leading '/') and build with '/'
+ * @property {string} name file name inside of the repository
+ *
+ * @param {string} name file name inside of the repository
+ */
+export class BaseEntry {
+  constructor(name) {
+    if (name[0] === "/" || name.indexOf("\\") >= 0) {
+      throw new TypeError(
+        `Names should not contain leading '/' or any '\\': ${name}`
+      );
+    }
+
+    Object.defineProperties(this, {
+      name: { value: name }
+    });
+  }
+
+  /**
+   *
+   * @return {string[]} UTI types
+   */
+  async getTypes() {
+    return [];
+  }
+
+  get isCollection() {
+    return false;
+  }
+
+  get isBlob() {
+    return false;
+  }
+
+  toJSON() {
+    return {
+      name: this.name
+    };
+  }
+}
