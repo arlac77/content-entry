dfb1ff936169952062492cdbc7c407c283ebdb3e
Markus Felten <Markus.Felten@db.com>
2018-12-13 21:24:55 +0100

    chore(rollup): declare externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,9 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import executable from "rollup-plugin-executable";
-import json from "rollup-plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import pkg from "./package.json";
 
-export default [
-  ...Object.keys(pkg.bin || {}).map(name => {
-    return {
-      input: `src/${name}-cli.js`,
-      output: {
-        file: pkg.bin[name],
-        format: "cjs",
-        banner:
-          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-worker "$0" "$@"',
-        interop: false
-      },
-      plugins: [
-        commonjs(),
-        json({
-          include: "package.json",
-          preferConst: true,
-          compact: true
-        }),
-        cleanup(),
-        executable()
-      ]
-    };
-  }),
+export default 
   {
     input: pkg.module,
     output: {
@@ -35,6 +11,7 @@ export default [
       format: "cjs",
       interop: false
     },
+    external: ["stream"],
     plugins: [resolve(), commonjs(), cleanup()]
   }
-];
+;
