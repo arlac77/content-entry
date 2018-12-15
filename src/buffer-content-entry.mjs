726ed77f572322e11e66007aa87561a210dbc628
Markus Felten <Markus.Felten@gmx.de>
2018-12-15 10:39:19 +0100

    feat: add BufferContentEntry

---
+++ src/buffer-content-entry.mjs
@@ -0,0 +1,32 @@
+
+import toReadableStream from "to-readable-stream";
+
+export function BufferContentEntryMixin(superclass) {
+  return class BufferContentEntryMixin extends superclass {
+  
+  get encoding() {
+    return "utf8";
+  }
+
+  /**
+   * Deliver content as string
+   * @return {string} content
+   */
+  async getString() {
+    return this.buffer.toString(this.encoding);
+  }
+
+  async getBuffer() {
+    return this.buffer;
+  }
+
+  /**
+   * Deliver content as read stream
+   * @return {ReadableStream} content
+   */
+  async getReadStream() {
+    return toReadableStream(this.buffer);
+  }
+
+  };
+}
