565f01c64e88063796e454d7928c6db8784f5672
Markus Felten <Markus.Felten@gmx.de>
2021-07-28 08:04:09 +0200

    fix: node / browser separation ...

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,5 @@
 export * from "./content-entry.mjs";
 export * from "./base-entry.mjs";
 export * from "./base-collection-entry.mjs";
-export * from "./deleted-content-entry.mjs";
 export * from "./stream-content-entry-mixin.mjs";
 export * from "./readable-stream-content-entry.mjs";
