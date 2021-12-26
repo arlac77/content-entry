19c85aa51fa9c2c7860a1e80a12288fc1c696b1c
Markus Felten <Markus.Felten@gmx.de>
2021-12-26 13:40:28 +0100

    fix: handle equalsContent undefined

--- src/content-entry.mjs
+++ src/content-entry.mjs
@@ -44,12 +44,16 @@ export class ContentEntry extends BaseEntry {
    * @return {Promise<boolean>} true if other has the same content (bitwise)
    */
   async equalsContent(other) {
+    if (other === undefined) {
+      return false;
+    }
+
     const [a, b] = await Promise.all([this.buffer, other.buffer]);
 
     if (a === undefined) {
       return b === undefined;
     }
-    
+
     return b === undefined ? false : a.equals(b);
   }
 
