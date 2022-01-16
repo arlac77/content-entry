a7e66ce2c2fa1b62a00989c7b65f4bc4130f1e8e
Markus Felten <Markus.Felten@gmx.de>
2022-01-16 21:37:37 +0100

    fix: node / browser separation

--- src/index-browser.mjs
+++ src/index-browser.mjs
@@ -1,6 +1,3 @@
 // @ts-check
-export * from "./content-entry.mjs";
 export * from "./base-entry.mjs";
 export * from "./base-collection-entry.mjs";
-export * from "./stream-content-entry-mixin.mjs";
-export * from "./readable-stream-content-entry.mjs";
