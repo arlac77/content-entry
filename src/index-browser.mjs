87845376e7939138596ac3a7a968444c52100cd7
Markus Felten <Markus.Felten@gmx.de>
2022-04-16 18:43:19 +0200

    fix: export buffer entries to browser too

--- src/index-browser.mjs
+++ src/index-browser.mjs
@@ -1,3 +1,5 @@
 // @ts-check
 export * from "./base-entry.mjs";
 export * from "./base-collection-entry.mjs";
+export * from "./buffer-content-entry-mixin.mjs";
+export * from "./buffer-content-entry.mjs";
