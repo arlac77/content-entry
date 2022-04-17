ea3045d90db6dfa6d2a5e57014d0c249e0c6c5b8
Markus Felten <Markus.Felten@gmx.de>
2022-04-17 21:23:15 +0200

    fix: export content-entry for browsers too

--- src/index-browser.mjs
+++ src/index-browser.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+export * from "./content-entry.mjs";
 export * from "./base-entry.mjs";
 export * from "./base-collection-entry.mjs";
 export * from "./buffer-content-entry-mixin.mjs";
